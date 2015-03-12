c21280bb26911ebcfe08f45cdd8cc68ed7c9b76d
Phil Schleihauf <uniphil@gmail.com>
2015-03-11 21:30:50 -0400

    reorderable grouping

--- js/components/overview.jsx
+++ js/components/overview.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var {addons: {update}} = require('react/addons');
 var {Link} = require('react-router');
 var TaskList = require('./task-list.jsx');
+var Icon = require('./icon.jsx');
 var actions = require('../actions');
 var aggregate = require('../aggregate');
 
@@ -17,7 +18,8 @@ var Grouper = React.createClass({
     return (
       <a
         key={this.props.id}
-        className={'grouper button' + (this.props.active ? ' inverse' : '')}
+        className={'grouper button' + (this.props.active ? ' inverse woo' : '')}
+        title={this.props.active ? 'enabled' : 'disabled'}
         onClick={this.toggle}>
         {this.props.name}
       </a>
@@ -68,15 +70,41 @@ var Query = React.createClass({
     this.setState(newState);
   },
 
+  swapGroupAround(i) {
+    var ab = asArrByWeight(this.state.group).splice(i, 2);
+    var u = {};
+    u[ab[0].id] = {weight: {$set: ab[1].weight}};
+    u[ab[1].id] = {weight: {$set: ab[0].weight}};
+    var newState = update(this.state, {group: u});
+    actions.query.set({
+      group: asArrByWeight(newState.group)
+        .filter((g) => g.active)
+        .map((g) => g.id)
+    });
+    this.setState(newState);
+  },
+
   render() {
+    var groupsByWeight = asArrByWeight(this.state.group);
     return (
       <div className="query">
         <strong>group</strong>
-        {asArrByWeight(this.state.group).map((g) => (
-          <Grouper
-            {...g}
-            key={g.id}
-            toggle={this.toggleGrouper} />))}
+        <span className="group buttons">
+          {groupsByWeight.map((g, i) => (
+            <span key={g.id}>
+              <Grouper
+                {...g}
+                toggle={this.toggleGrouper} />
+              {i < (groupsByWeight.length - 1) && (
+                <button
+                  className="swap button bare nofocus"
+                  title="swap order"
+                  onClick={() => this.swapGroupAround(i)}>
+                  <Icon id="swap" alt="swap grouping order" />
+                </button>)}
+            </span>
+          ))}
+        </span>
       </div>
     );
   },
